465560a36a88143b1e4437941b9e7d4a398ddf29
unknown <your@email.com>
2025-05-23 14:52:38 +0900

    삭제 기능 추가

--- newsblog/NewsBlog.js
+++ newsblog/NewsBlog.js
@@ -36,10 +36,64 @@ function NewsBlog(){
             </div>
 
             <div>
-                <input type="text" onChange={(event)=>{
-                    console.log(event);
+                <input type="text" id="input news" value={inputText} onChange={(event)=>{
+                    //console.log(event);
+                    console.log(event.target.value);
+                    setInputText(event.target.value);
                 }}></input>
                 <button onClick={()=>{
+                    //입력된 값을 제목 배열에 추가
+                    //입력된 값 : input 요소에서 onChange 가 발생할때 ->inputText state변수에 저장되어 있음
+                    //제목배열 news 배열변수에 추가 -> 리렌더링 -> news배열의 갯수만큼 반복(map) 화면에 표시
+
+                    //입력값이 있는 경우만~
+                    /*
+                    if(inputText.trim().length > 0){
+                        let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+                    
+                    //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 ->set함수 원래 배열에 저장
+                    setInputText(''); //공백 -> input value={inputText}
+                    } else {
+                        alert("내용을 입력 후 등록하세요~");
+                        setInputText('');
+                    }*/
+
+
+                    //inputText = ' abc';
+
+                    //if(inputText.trim().length == 0){
+
+                    inputText = inputText.trim();
+                    if(inputText.length == 0){
+                    alert("내용을 입력 후 등록하세요~");
+                        setInputText('');
+                        return;
+                    }
+
+                    let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+
+                    likeCountArr.push(0);
+
+                    //배열state변수 바꾸는 법 : 배열 복사 -> 값 변경/추가/삭제 -> set함수로
+                    setInputText(''); //공백 -> input value={inputText}
+
+
+
+
+                    /*
+
+                    let title = document.getElementById('input_news').value;
+                    let temp = [...news];
+                    temp.push(inputText);
+                    setNews(temp);
+
+                    document.getElementById('input_news').value = '';
+
+                    */
 
                 }}>등록</button>
             </div>
@@ -48,7 +102,7 @@ function NewsBlog(){
         news.map((item, index)=>{ // 0 1 2
             //return
             return(
-            <div className='post-list'>
+            <div className='post-list' key={index}>
                 <h4 onClick={()=>{
                         setShowModal(!showModal);
                     setSelectedTitle(item);
@@ -62,7 +116,15 @@ function NewsBlog(){
                     setLikeCountArr(temp);
 
                 }}>❤</span>{likeCountArr[index]}</h4>
-                <p>내용 무</p>
+                <p>내용 무</p>  
+                <button onClick={()=>{
+                    let temp = [...news];
+                    temp.splice(index, 1);
+                    setNews(temp);
+
+                    likeCountArr.splice(index, 1);
+                    
+                }}>삭제</button>
             </div>
             )
 
