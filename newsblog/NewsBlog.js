0ab6115ea6a361038b2f85b8ca2e2238c23c2c0b
unknown <your@email.com>
2025-05-23 12:05:27 +0900

    map 반복처리, props 전달(변수, 함수)

--- newsblog/NewsBlog.js
+++ newsblog/NewsBlog.js
@@ -1,45 +1,66 @@
 import './NewsBlog.css';
 import { useState } from 'react';
+import Modal from './Modal';
 
 function NewsBlog(){
 
-    let title = "React Study"
+    let title = "React Study";
 
     let [news1, setNews1] = useState('오늘의 뉴스');
     let [news2, setNews2] = useState('어제의 뉴스');
     let [news3, setNews3] = useState('내일의 뉴스');
 
     let [news, setNews] = useState(['오늘의 뉴스', '어제의 뉴스', '내일의 뉴스']);
+                //                 ['Today News', '어제의 뉴스', '내일의 뉴스']
 
-    let [likeCount, setLikeCount] = useState(0);
+    let [likeCount1, setLikeCount1] = useState(0);
+    let [likeCount2, setLikeCount2] = useState(0);
+    let [likeCount3, setLikeCount3] = useState(0);
 
-    return(
+    let [likeCountArr, setLikeCountArr] = useState( [0, 0, 0] );
+
+    let [showModal, setShowModal] = useState(false);
+    //    0  1 
+
+    let [selectedTitle, setSelectedTitle] = useState('');
+    let [selectedLikeCount, setselectedLikecount] = useState(0);
+
+    return (
         <div>
             <div className='black-nav'>
-            <h4>Blog Header</h4>
-            <p style={ {color:"orange", fontSize:'20px'}}>{title}</p>
-        </div>
+                <h4>Blog Header</h4>
+                <p style={ {color:"orange", fontSize:'20px'} }>{title}</p>
+            </div>
 
-        <div className='post-list'>
-            {/* <h4>{news1}</h4>} */}
-            <h4>{news[0]} <span onClick={ ()=>{
-                setLikeCount(likeCount+1);
-            }}>🖤</span>{likeCount}</h4>
-            <p>내용 무</p>
-            <button>제목 변경</button>
-        </div>
+        {
+        news.map((item, index)=>{
+            //return
+            return(
+            <div className='post-list'>
+                <h4 onClick={()=>{
+                        setShowModal(!showModal);
+                    setSelectedTitle(item);
+                }}>{item} <span onClick={(event)=>{
+                    
+                    event.stopPropagation();
 
-        <div className='post-list'>
-            <h4>{news[1]}</h4>
-            <p>내용 무</p>
+                    let temp = [...likeCountArr];
+                    temp[index] += 1;
+                    setLikeCountArr(temp);
 
-        </div>
+                }}>❤</span>{likeCountArr[index]}</h4>
+                <p>내용 무</p>
+            </div>
+            )
 
-        <div className='post-list'>
-            <h4>{news[2]}</h4>
-            <p>내용 무</p>
-        </div>
+        })
+        }
 
+            {
+                //showModal == true ? <Modal/> : null
+                showModal && <Modal title={selectedTitle} likeCount={selectedLikeCount}/>
+            }
+            
         </div>
     );
 }
