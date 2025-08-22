8174a99deb8cedabaef05f6beddbe59acfb08d23
song373 <song120437@naver.com>
2025-08-22 14:44:41 +0900

    리덕스(reduxtoolkit) 설정 및 사용방법

--- react-study1/src/index.js
+++ react-study1/src/index.js
@@ -5,15 +5,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Temp from './Temp.js';
 import { BrowserRouter } from 'react-router';
+import {Provider} from 'react-redux';
+import store from './react_spring/store/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  //<React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </Provider>
+
     /* <Temp/> */
- // </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
